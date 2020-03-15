2ebbd64064d9c6d45d2b70bccef6125b2f127f27
WoojtekP <pawlik.wj@gmail.com>
2020-03-15 16:28:42 +0100

    generator with simple interface

---
+++ reasoner.cpp
@@ -0,0 +1,82 @@
+#include "reasoner.hpp"
+
+namespace reasoner {
+    int game_state::get_current_player(void) const {
+        return current_player;
+    }
+
+    int game_state::get_player_score(int player_id) const {
+        return variables[player_id-1];
+    }
+
+    void game_state::apply_move(const uint& move_id) {
+        auto move_mask = all_moves[move_id].mr;
+        uint64_t tokens = (current_player == YELLOW) ? yellow : red;
+
+        tokens |= move_mask;
+
+        static constexpr line_matcher m[] = {
+            {0x78F1E3C78Full, 1, 2, 3},  // row
+            {0x1FFFFFull, 7, 14, 21},  // column
+            {0x1E3C78ull, 6, 12, 18},  // left diagonal
+            {0x3C78Full, 8, 16, 24}  // righ diagonal
+        };
+
+        for (int i = 0; i < 4; ++i) {
+            if (tokens & (tokens >> m[i].s1) & (tokens >> m[i].s2) & (tokens >> m[i].s3) & m[i].mask) {
+                size = 0;
+                return;
+            }
+        }
+
+        move_mask <<= 7;
+        if (move_mask >= (1ull << 42)) {
+            if (size == 1) {
+                size = 0;
+                return;
+            }
+            all_moves[move_id].mr = all_moves.back().mr;
+            size--;
+        }
+        else {
+            all_moves[move_id].mr = move_mask;
+        }
+
+        if (current_player == RED) {
+            red = tokens;
+            current_player = YELLOW;
+        }
+        else {
+            yellow = tokens;
+            current_player = RED;
+        }
+    }
+
+    std::vector<move> game_state::get_all_moves(resettable_bitarray_stack& cache) {
+        std::vector<move> result;
+        result.reserve(100);
+        get_all_moves(cache, result);
+        return result;
+    }
+
+    void game_state::get_all_moves(resettable_bitarray_stack&, std::vector<move>&) {
+        return;
+    }
+
+    bool game_state::apply_any_move(resettable_bitarray_stack&) {
+        return false;
+    }
+
+    int game_state::get_monotonicity_class(void) {
+        return -1;
+    }
+
+    bool game_state::is_legal([[maybe_unused]] const move& m) const {
+        return false;
+    }
+
+    uint game_state::get_moves_count() const {
+        return size;
+    }
+
+}
---
+++ reasoner.hpp
@@ -0,0 +1,69 @@
+#ifndef REASONER
+#define REASONER
+
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdint>
+#include <string>
+#include <vector>
+
+namespace reasoner {
+    constexpr int NUMBER_OF_PLAYERS = 3;
+    constexpr int MONOTONIC_CLASSES = 0;
+
+    class resettable_bitarray_stack {};
+
+    typedef uint64_t move_representation;
+    struct move {
+        move_representation mr;
+        move(const move_representation& mv) : mr(mv) {};
+        move(void) = default;
+        bool operator==(const move& rhs) const {
+            return mr == rhs.mr;
+        }
+    };
+    enum player {
+        KEEPER = 0,
+        RED = 1,
+        YELLOW = 2
+    };
+    struct line_matcher {
+        uint64_t mask;
+        uint8_t s1;
+        uint8_t s2;
+        uint8_t s3;
+    };
+    class game_state {
+        public:
+            int get_current_player(void) const;
+            int get_player_score(int player_id) const;
+            void apply_move(const move&);
+            void apply_move(const uint&);
+            std::vector<move> get_all_moves(resettable_bitarray_stack& cache);
+            void get_all_moves(resettable_bitarray_stack&, std::vector<move>& moves);
+            bool apply_any_move(resettable_bitarray_stack&);
+            int get_monotonicity_class(void);
+            bool is_legal(const move& m) const;
+            uint get_moves_count() const;
+        private:
+            uint64_t red = 0;
+            uint64_t yellow = 0;
+            uint size = 7;
+            int current_cell = 1;
+            player current_player = RED;
+            int current_state = 0;
+            int variables[2] = {0, 0};
+            std::vector<move> all_moves {
+                1ull,
+                1ull << 1,
+                1ull << 2,
+                1ull << 3,
+                1ull << 4,
+                1ull << 5,
+                1ull << 6,
+            };
+    };
+}
+
+#endif
